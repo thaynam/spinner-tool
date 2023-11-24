c1cd79b0ae711f54871ea0770d7af8ede3090ed2
thaynam <lazaro@thaynam.com>
2023-11-24 16:14:05 -0300

    add restart database and reset

--- spinner-tool.sh
+++ spinner-tool.sh
@@ -53,7 +53,7 @@ then
 elif [ $OPTION == "build" ]; then
 	echo "spinner $OPTION"
 	ENV_NAME=${ENV_NAME:-e5a2preprod}
-	RELEASE_VERSION=${RELEASE_VERSION:-u98}
+	RELEASE_VERSION=${RELEASE_VERSION:-u102}
 	DXP_ACTIVATION_KEY_DIR=${DXP_ACTIVATION_KEY_DIR:-~/dev/projects/dxp-activation-key/.}
 	
 	echo "";
@@ -69,65 +69,102 @@ elif [ $OPTION == "build" ]; then
 
 	if [ ! -d "~/dev/projects" ] ; then
 		cd ~/
-		mkdir dev/projects
+		mkdir dev/projects >/dev/null
 	fi
 
 	if [ ! -d "$LIFERAY_DOCKER_DIR" ] ; then
-		git clone $LIFERAY_DOCKER_REMOTE $LIFERAY_DOCKER_DIR
+		git clone $LIFERAY_DOCKER_REMOTE $LIFERAY_DOCKER_DIR >/dev/null
 		cd $LIFERAY_DOCKER_DIR
 		git pull 
-		git reset HEAD --hard
 		git checkout master
+		git rm .gitattributes >/dev/null
+		git add -A
+		git reset --hard
 
 	else
 		cd $LIFERAY_DOCKER_DIR
 		git pull 
-		git reset HEAD --hard
 		git checkout master
+		git rm .gitattributes >/dev/null
+		git add -A
+		git reset --hard
 	fi
-
+	printf '\n'
 	## create or  update liferay-lxc
 	LIFERAY_LXC_DIR=~/dev/projects/liferay-lxc
 	LIFERAY_LXC_REMOTE=git@github.com:liferay/liferay-lxc.git
 
 	if [ ! -d "$LIFERAY_LXC_DIR" ] ; then
-		git clone $LIFERAY_LXC_REMOTE $LIFERAY_LXC_DIR
-		cd $LIFERAY_LXC_DIR
+		git clone $LIFERAY_LXC_REMOTE $LIFERAY_LXC_DIR >/dev/null
+		cd $LIFERAY_LXC_DIR 
 		git pull 
-		git reset HEAD --hard
 		git checkout 7.4-$RELEASE_VERSION
+		git rm .gitattributes >/dev/null
+		git add -A
+		git reset --hard >/dev/null
 
 	else
 		cd $LIFERAY_LXC_DIR
 		git pull 
-		git reset HEAD --hard
 		git checkout 7.4-$RELEASE_VERSION
+		git rm .gitattributes >/dev/null
+		git add -A
+		git reset --hard >/dev/null
 	fi
 	
-	## stop and remove all mysql dockers
-	# docker rm $(docker stop $(docker ps -a -q --filter="expose=3306"))
 	## stop and remov	e all dockers with env name
-	docker rm $(docker stop $(docker ps -a -q --filter="name=env-$ENV_NAME*"))
+	docker rm $(docker stop $(docker ps -a -q --filter="name=env-$ENV_NAME*") -t 2)
 	## copy dxp-activation-key to spinner directory
 	rsync -avz $DXP_ACTIVATION_KEY_DIR ~/dev/projects/liferay-docker/spinner/dxp-activation-key 
 	##	remove old cluster dir
 	cd ~/dev/projects/liferay-docker/spinner
-	rm -rf ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/
+	find . -name "env-*" -type d -prune -exec rm -rf '{}' +
 	## build spinner 
 	./build.sh $ENV_NAME
 	cd ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/
 	## create spinner dockers
-	docker-compose up -d antivirus database search && docker-compose up liferay-1	
+	docker compose up -d antivirus database search && docker compose up liferay-1	
 
 elif [ $OPTION == "start" ]; then
 	echo "spinner $OPTION"
-	docker start $(docker ps -a -q --filter="name=env-$ENV_NAME*")
+	cd ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/
+	docker compose start
+	printf '\n'
+	printf "Starting spinner..."
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '\n'
+
 	LIFERAY_ID=$(docker ps -a -q --filter="publish=18080")
 	docker logs -f $LIFERAY_ID
 
 elif [ $OPTION == "stop"  ]; then
 	echo "spinner $OPTION"
-	docker stop $(docker ps -a -q --filter="name=env-$ENV_NAME*")
+	cd ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/
+	docker compose stop
+
+elif [ $OPTION == "restart"  ]; then
+	echo "spinner $OPTION"
+	cd ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/
+	docker compose restart
+	printf '\n'
+	printf "Starting spinner..."
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '\n'
+
+	LIFERAY_ID=$(docker ps -a -q --filter="publish=18080")
+	docker logs -f $LIFERAY_ID
 
 elif [ $OPTION == "rm"  ]; then
 	echo "spinner $OPTION"
@@ -148,10 +185,33 @@ elif [ $OPTION == "deployDev"  ]; then
 	LIFERAY_ID=$(docker ps -a -q --filter="publish=18080")
 	gw deployDev -Ddeploy.docker.container.id=$LIFERAY_ID
 
-elif [ $OPTION == "deployStaging"  ]; then
+elif [ $OPTION == "database"  ]; then
 	echo "spinner $OPTION"
-	LIFERAY_ID=$(docker ps -a -q --filter="publish=18080")
-	gw deployStaging -Ddeploy.docker.container.id=$LIFERAY_ID
+	DATABASE_PASSWORD="password"
+	cd ~/dev/projects/liferay-docker/spinner/env-$ENV_NAME*/	
+	docker compose stop search antivirus liferay-1
+	docker compose start database
+
+	DATABASE_ID=$(docker ps -a -q --filter="name=database_")
+   printf '\n'
+
+	printf 'Starting database clean up'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '.'
+    sleep .5
+    printf '\n'
+ 
+	docker exec -it $DATABASE_ID mysqladmin -p$DATABASE_PASSWORD drop lportal create lportal -f 
+ 
+elif [ $OPTION == "reset"  ]; then
+	echo "spinner $OPTION"
+	rm -rf ~/dev/projects/liferay-docker
+	rm -rf ~/dev/projects/liferay-lxc
+	printf 'liferay-docker and liferay-lxc repositories were deleted.'
 else
-	echo -e "Choose an option:  \n- spinner build \n- spinner start \n- spinner stop \n- spinner rm \n- spinner forceDeploy \n- spinner deploy "
+	echo -e "Choose an option:  \n- spinner build \n- spinner start \n- spinner stop  \n- spinner restart \n- spinner rm \n- spinner deploy \n- spinner deployDev \n- spinner database \n- spinner reset"
 fi
